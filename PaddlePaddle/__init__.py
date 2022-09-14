23fc9425f2ab055fc129eb1346681323b42a898e
juyi <3323194808@qq.com>
2022-09-14 21:14:29 +0800

    更新

--- PaddlePaddle/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding = utf-8 -*-
-# @Time : 2022/7/26 20:31
-# @Author : Juyi
-# @File : __init__.py.py
-# @Software : PyCharm
--- dataAnalysis/Demo01.py
+++ dataAnalysis/Demo01.py
@@ -8,4 +8,3 @@
 import matplotlib.pyplot as plt
 plt.rcParams['font.sans-serif']=['SimHei'] # 用来正常显示中文　
 # plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
-print("he")
