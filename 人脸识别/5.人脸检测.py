f09888a3b97e1c77f80f7166c0662649ace7756b
juyi <3323194808@qq.com>
2022-10-03 14:52:45 +0800

    视频检测

--- 人脸识别/5.人脸检测.py
+++ 人脸识别/5.人脸检测.py
@@ -12,13 +12,15 @@ def face_detectDemo(img):
     # 加载分类器（做出人脸识别的关键，调用cv里的分类器）
     face_detect = cv.CascadeClassifier(
         'E:/Anaconda/envs/juyi/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
-    face = face_detect.detectMultiScale(gray,1.1)  # 多尺度检测  (传入灰度图)
+    face = face_detect.detectMultiScale(gray)  # 多尺度检测  (传入灰度图)
+    # gray, 1.05,5, 0, (10, 10), (50,50)
     for x, y, w, h in face:
         cv.rectangle(img, (x, y), (x + w, y + h), color=(0, 0, 255), thickness=1)
     cv.imshow('result', img)
 
 
-img = cv.imread("11.jpg")
+img = cv.imread("111.jpg")
+# img = cv.resize(img, (1063, 554))
 face_detectDemo(img)
 # 等待(检测到q后退出)
 while True:
--- 人脸识别/6.检测多个.py
+++ 人脸识别/6.检测多个.py
@@ -5,20 +5,21 @@
 # @Software : PyCharm
 import cv2 as cv
 
+
 # 检测人脸的函数
 def face_detectDemo(img):
     gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)  # 转换为灰度图
     # 加载分类器（做出人脸识别的关键，调用cv里的分类器）
     face_detect = cv.CascadeClassifier(
         'E:/Anaconda/envs/juyi/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
-    face = face_detect.detectMultiScale(gray,1.1)  # 多尺度检测  (传入灰度图)
+    face = face_detect.detectMultiScale(gray, 1.1)  # 多尺度检测  (传入灰度图)
     for x, y, w, h in face:
-        cv.rectangle(img, (x, y),( x + w, y + h), color=(0, 0, 255), thickness=1)
+        cv.rectangle(img, (x, y), (x + w, y + h), color=(0, 0, 255), thickness=1)
     cv.imshow('result', img)
 
 
-img = cv.imread("11.jpg")
-
+img = cv.imread("111.jpg")
+# img = cv.resize(img, (960, 540))
 face_detectDemo(img)
 # 等待(检测到q后退出)
 while True:
