66bf1446d56f16a04f191f99b388f502d8a26b36
juyi <3323194808@qq.com>
2022-10-07 20:43:46 +0800

    数据读取

--- 人脸识别/8.人脸录入.py
+++ 人脸识别/8.人脸录入.py
@@ -5,18 +5,19 @@
 # @Software : PyCharm
 
 import cv2 as cv
+
 # 获取摄像头，传入0表示获取系统默认摄像头
 cap = cv.VideoCapture(0)
 flag = 1
-num =1
+num = 1
 
-while(cap.isOpened()):  # 检测摄像头开启状态
+while (cap.isOpened()):  # 检测摄像头开启状态
     ret_flag, Vshow = cap.read()
     cv.imshow("Capture_Test", Vshow)
     k = cv.waitkey(1) & 0xFF  # 按键中断
-    if k ==ord('s'):
-        cv.imwrite("C:/Users/Judy/Desktop/save_Test/" + str(num)+".name"+".jpg",Vshow)  # 保存格式（name可以自设定）
-        print("Success to save!"+str(num)+".jpg")
+    if k == ord('s'):
+        cv.imwrite("C:/Users/Judy/Desktop/save_Test/" + str(num) + ".name" + ".jpg", Vshow)  # 保存当前帧图像格式（name可以自设定）
+        print("Success to save!" + str(num) + ".jpg")
         print("--------------------------")
         num += 1
     elif k == ord(' '):
---
+++ 人脸识别/9.数据训练.py
@@ -0,0 +1,18 @@
+# -*- coding = utf-8 -*-
+# @Time : 2022/10/7 20:15
+# @Author : Juyi
+# @File : 9.数据训练.py
+# @Software : PyCharm
+import os
+import cv2 as cv
+from PIL import Image
+import numpy as np
+
+def getImageLabel(path):
+
+
+
+if __name__ == "__main__":
+    # 要训练的图片路径
+    path = ''
+
